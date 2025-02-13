20532489ba0a8ebcf292bcb1a7610dc1678b8923
sudhi12337 <sudheerdarur@gmail.com>
2025-02-12 22:22:29 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-function showAlert(){
-    alert('hello! the button is clicked,this js class')
+// counter app
 
+document.addEventListener("counter",init);
+
+let count =  Number(sessionStorage.getItem("count")) || 0;
+
+function init() {
+    document.getElementById("count").textContent = count;
 }
 
-function changetext(){
-    document.getElementById('text').innerHTML="text changed" 
+function increasecount() {
+   count++;
+   updateCount(); 
+}
+function decreasecount() {
+    count--;
+    updateCount(); 
+}
+function resetcount() {
+    count = 0;
+    updateCount();
 }
 
-function handleclick(){
-    document.getElementById('clickmessage').innerHTML="button clicked" 
-}
-function validateform(){
-    let name=document.getElementById('name').Value;
-    if(name==''){
-        alert('please enter your name.');
-        return false;
-    }
-    return true;
-
-}
-function checknumber(){
-    let num=parseInt(document.getElementById('numberinput').value);
-    let result=num>10 ? 'greater than 10' : '10 or less';
-    document.getElementById('numberresult').innerHTML=result;
-
-
-}
-function checkage(){
-    let age=parseInt(document.getElementById('ageinput').value);
-    if(age >= 18)
-    document.getElementById('ageresult').innerHTML='you are an adult';
-else{
-    document.getElementById('ageresult').innerHTML='you are a minor';
-}
-}
-function displayarrayitem(){
-    let item=['appale','banana','charry','date','elderbarry'];
-    let index=parseInt(document.getElementById('arrayindex').value);
-    let result=items[index] || 'invalid index';
-    document.getElementById('arrayresult').innerHTML=result;
+function updateCount(){
+    document.getElementById("count").textContent = count;
+    sessionStorage.setItem("count",count);
 }
